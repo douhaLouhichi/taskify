530bfdc3f28bc7d735cee5059b168ef1dcbea0f2
douhaLouhichi <douhalou4@gmail.com>
2022-08-06 22:47:57 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React from 'react';
+
+import logo from './logo.svg';
+import { Home } from './pages/Home';
+
+function App() {
+    return (
+        <div className='App'>
+            <Home />
+        </div>
+    );
+}
+
+export default App;
